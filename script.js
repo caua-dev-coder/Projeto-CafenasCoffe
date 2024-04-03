1b5ccadfee21095ba1c9bffbcd1b5ad91432e521
caua-dev-coder <cauatavellaprofissional@gmail.com>
2024-04-03 17:53:27 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal(".home", {duration: 1500})
+sr.reveal(".about", {duration: 1500})
+sr.reveal(".menu", {duration: 1500})
+sr.reveal(".review", {duration: 1500})
+sr.reveal(".address", {duration: 1500})
